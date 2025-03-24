5d07c2e38f150e51695e09b91166bf71735f65bf
taoyan6666 <60025008+taoyan6666@users.noreply.github.com>
2025-03-24 16:24:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
